0e08426ed902195ed868c2d4a67ab1f5741086df
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-30 13:39:22 +0300

    Users сага и экшн-creator в уточке

---
+++ src/ducks/users/actions.js
@@ -0,0 +1,5 @@
+import {createActions} from 'redux-actions';
+
+const { fetchTokenOwner } = createActions({
+  FETCH_TOKEN_OWNER: undefined
+});
---
+++ src/sagas/users.js
@@ -0,0 +1,10 @@
+import { takeEvery } from 'redux-saga';
+// import {  } from '../ducks'
+
+export const fetchUserWatch = function* () {
+  yield takeEvery();
+};
+
+const fetchUserSaga = function * () {
+
+};
