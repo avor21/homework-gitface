ae5ae1bdb7af38f71a6e6f88ebd4311b3bee4a7f
Alina Voronina <skorokhodova.a5@gmail.com>
2020-02-01 00:27:34 +0300

    Наполнение страницы пользователя

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {
+  fetchUserRequest,
+  getIsFetched,
+  getIsFetching,
+  getUserInfo
+} from "../../ducks/users";
+import {Header, Image, Label, Segment} from "semantic-ui-react";
 
-const UserPage = () => {
-  return <h2>User page</h2>;
-};
+class UserPage extends Component{
+  componentDidMount() {
+    const { fetchUserRequest, isFetched } = this.props;
+    isFetched && fetchUserRequest();
+  }
 
-export default UserPage;
+  render() {
+    const { isFetching} = this.props;
+
+    return (
+      <Segment placeholder>
+        { isFetching ? this.renderPreloader() : this.renderUserCard() }
+
+      </Segment>
+    );
+  }
+
+  renderUserCard = () => {
+    const { login, followers, following, avatar_url: avatar } = this.props.user;
+    return (
+      <div>
+        <Image src={avatar} size="medium" circular floated='left' />
+        <Header as="h2">{login}</Header>
+        <Label>
+          Followers
+          <Label.Detail>{followers}</Label.Detail>
+        </Label>
+        <Label>
+          Following
+          <Label.Detail>{following}</Label.Detail>
+        </Label>
+      </div>
+    );
+  };
+
+  renderPreloader = () => <p>Загрузка...</p>;
+
+}
+
+export default connect(state => ({
+  isFetching: getIsFetching(state),
+  isFetched: getIsFetched(state),
+  user: getUserInfo(state)
+}), { fetchUserRequest })(UserPage);
---
+++ src/ducks/users/selectors.js
@@ -0,0 +1,5 @@
+export const getUserInfo = state => state.users.data;
+export const getIsFetching = state => state.users.isFetching;
+export const getIsFetched = state => state.users.isFetched;
+
+
